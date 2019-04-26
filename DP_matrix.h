9895e90ccb4e2159d0569ed5f3cc2bba540e49f8
rebeccaserramari <38427001+rebeccaserramari@users.noreply.github.com>
2019-04-26 15:56:21 +0200

    Add files via upload

--- DP_matrix.h
+++ DP_matrix.h
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <set>
+#include <unordered_set>
 #include <climits>
 #include <iostream>
 #include <memory>
@@ -15,13 +16,17 @@
 #include <tuple>
 #include <fstream>
 #include <math.h>
+#include <cmath>
+#include <map>
 #include "boost/multi_array.hpp"
 #include <cassert>
 #include <time.h>
-float** printlist(std::vector<int>, std::vector<int>, std::vector<std::vector<int>>, float, std::set<int> );
+float** perform_DP(std::vector<int>, std::vector<int>, std::vector<std::vector<int>>, float, std::unordered_set<int> );
+//std::tuple<float**, std::vector<int>> perform_DP(std::vector<int>, std::vector<int>, std::vector<std::vector<int>>, float, std::unordered_set<int> );
+//std::tuple<float**,int> DP_partial(std::vector<int> , std::vector<int> , std::vector<std::vector<int>> , float, int, int, float**, std::unordered_set<int> , std::map<int,int> );
+//std::tuple<std::vector<int>, std::vector<int>> backtrace_partial(float** , int, int, float**, std::unordered_set<int>, std::map<int,int>, int);
 float mut(int, int, int, int, float);
 void compute_scoring(char*, char*, char*, float, char*, char*);
 std::tuple<float, int> minimum(float*, int, int, bool, int);
-//void compute_score(std::vector<int>, std::vector<int>, std::set<int>, std::vector<std::vector<int> > , std::string, float, float);
 
 #endif
